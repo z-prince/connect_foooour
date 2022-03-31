cb4784e44961f809fc7199bcec453a682c3db0b6
Zach Prince <zachary.s.prince@outlook.com>
2022-03-31 13:35:32 -0400

    Add comments

--- lib/board.rb
+++ lib/board.rb
@@ -20,10 +20,10 @@ class Board
 def print_board
   puts @board.keys.join #A, B, C, D, E, E, F, G
     6.times do |e|
-        @board.keys.each do |key|
+        @board.keys.each do |key| # Takes each key and prints it's values 6 times 
         print @board[key][e]
         end
-    puts ''
+    puts '' # Creates a break in the lines to convey a proper board
     end
   return
 end
