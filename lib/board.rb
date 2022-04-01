496efea633bef7b11017f6c7b441d9eac08d75c3
Zach Prince <zachary.s.prince@outlook.com>
2022-04-01 13:10:41 -0400

    rebase code

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# class Board will create and interact with the board.
 class Board
   attr_reader :board
 
@@ -16,11 +19,10 @@ class Board
   def print_board
     puts @board.keys.join # A, B, C, D, E, E, F, G
     6.times do |e|
-      @board.keys.each do |key| # Takes each key and prints it's values 6 times
+      @board.each_key do |key| # Takes each key and prints it's values 6 times
         print @board[key][e]
       end
-      puts '' # Creates a break in the lines to convey a proper board
+        puts '' # Creates a break in the lines to convey a proper board
     end
-    nil
   end
 end
