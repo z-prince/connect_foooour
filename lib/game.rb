3b39dbd22704eb0c84c163756c04a912fd4282ed
Zach Prince <zachary.s.prince@outlook.com>
2022-04-05 12:05:12 -0400

    refactor: game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -4,53 +4,52 @@ require './lib/player'
 require 'pry'
 # This class will allow Connect-4 to be played
 class Game
-    attr_reader :player
+  attr_reader :player
 
-    def initialize
+  def initialize
         # board = Board.new
         @player = Player.new
-    end
+  end
 
-    def greeting
-        p 'Welcome to CoNnEcT fOoOoUr!'
-        p 'The wacky contest where vertical, horizontal, and diagonal domination are the name of the game!'
-        p 'May the odds be ever in your favor!'
-        p ''
-        p 'Enter p to play. Enter q to quit.'
-        choice = gets.chomp.downcase
-        if choice == 'p'
-            # is this a body
-        else
-            p "You're really missing out!"
-        end
+  def greeting
+    p 'Welcome to CoNnEcT fOoOoUr!'
+    p 'The wacky contest where vertical, horizontal, and diagonal domination are the name of the game!'
+    p 'May the odds be ever in your favor!'
+    p ''
+    p 'Enter p to play. Enter q to quit.'
+    choice = gets.chomp.downcase
+    if choice == 'p'
+    # is this a body
+    else
+      p "You're really missing out!"
     end
+  end
 
 # --------------- WIN CONDITIONS ---------------
-      
-    def draw
-        if @player.session.space.values == [-1, -1, -1, -1, -1, -1, -1]
-            puts "It's a DRAW!!!"
-            exit
+    
+  def draw
+    if @player.session.space.values == [-1, -1, -1, -1, -1, -1, -1]
+      puts "It's a DRAW!!!"
+      exit
         end
     end
 
-    def check_horizontal; end
+  def check_horizontal; end
 
-    def check_vertical
-        if  @player.session.board.each_value do |win| 
-                # binding.pry
-                 win == %w[. . X X X X] || %w[. X X X X .] || %w[X X X X . .]
-            end
-        end
+  def check_vertical
+    if  @player.session.board.each_value do |win| 
+    # binding.pry
+    win == %w[. . X X X X] || %w[. X X X X .] || %w[X X X X . .]
+      end
     end
+  end
 
-    def check_diagonal; end
+  def check_diagonal; end
 
-    def triumph
-        if check_vertical == true
-            puts "Player WINS"
-            exit
-        end
+  def triumph
+    if check_vertical == true
+      puts 'Player WINS'
+      exit
     end
 end
 
